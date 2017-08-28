ed00cd4328f17dfbe781534dc50c9dec04f02b84
Laurent Forthomme <laurent.forthomme@cern.ch>
2017-08-28 15:11:35 +0200

    Slightly modified the includes structure

--- Examples/fpmc_bare.cc
+++ Examples/fpmc_bare.cc
@@ -6,18 +6,18 @@ using namespace std;
 
 int main( int argc, char* argv[] )
 {
-  //fpmc::Fpmc gen( argv[1] );
-  fpmc::Fpmc gen;
+  fpmc::Fpmc gen( argv[1] );
+  /*fpmc::Fpmc gen;
   gen.parameters().setSqrtS( 13.e3 ); // in GeV
   gen.parameters().setProcessId( 16008 ); // QED yy->mu,mu
   gen.parameters().setProcessType( fpmc::ExclusiveProcess );
   gen.parameters().setInteractionType( fpmc::QED );
   gen.parameters().setIntermediateFlux( fpmc::PhotonPhotonBudnevCoherent );
-  gen.parameters().setPtRange( 10. ); // no upper limit
+  gen.parameters().setPtRange( 10. ); // no upper limit*/
 
   gen.parameters().dump();
 
-  for( unsigned int evt = 0; evt < 10e5; ++evt ) {
+  for( unsigned int evt = 0; evt < 1e4; ++evt ) {
     cout << "[FPMC] Processing event " << ( evt+1 ) << endl;
     gen.next();
   }
